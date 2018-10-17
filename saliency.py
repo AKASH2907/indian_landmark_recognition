b1f9c1050fc2031efa2d532bdcbf8ce57a67476e
AKASH2907 <kmr2907akash@gmail.com>
2018-10-17 23:16:22 +0530

    code updates

--- saliency.py
+++ saliency.py
@@ -10,104 +10,53 @@ from os import rename, listdir, rename, makedirs
 
 monuments = ["Buddhist", "Dravidian", "Kalinga", "Mughal"]
 
-# destination = '../crops_txt/'
+destination = '../cropped_images/'
 datapath = '../train_data/'
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", required=True,
-	help="path to BING objectness saliency model")	
-ap.add_argument("-n", "--max-detections", type=int, default=20,
+# ap.add_argument("-m", "--model", required=True,
+# 	help="path to BING objectness saliency model")	
+ap.add_argument("-n", "--max-detections", type=int, default=3,
 	help="maximum # of detections to examine")
 args = vars(ap.parse_args())
 
 # initialize OpenCV's objectness saliency detector and set the path
 # to the input model files
 saliency = cv2.saliency.ObjectnessBING_create()
-saliency.setTrainingPath(args["model"])
+# saliency.setTrainingPath(args["model"])
+saliency.setTrainingPath('./ObjectnessTrainedModel')
 
 
 for i in monuments:
 	path = join(datapath, i)
 	files = listdir(path)
 	files.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-	# txt_path = join(datapath, i)
+
+	cropped_path = join(destination, i)
+	j=0
 	for image in files:
 		im = join(path, image)
-
 		read = cv2.imread(im, 1)
 
-
-		print(read.shape)
 		# compute the bounding box predictions used to indicate saliency
 		(success, saliencyMap) = saliency.computeSaliency(read)
 		numDetections = saliencyMap.shape[0]
 
 		# loop over the detections
-		# points = []
+		
 		for i in range(0, min(numDetections, args["max_detections"])):
 			# extract the bounding box coordinates
 			
 			(startX, startY, endX, endY) = saliencyMap[i].flatten()
-			
-			print(startX, startY, endX, endY)
-			print(type(startX))
-			# points+=[[startX, endX, startY, endY]]
-			# print(points)
-			# randomly generate a color for the object and draw it on the image
-			output = read.copy()
-			color = np.random.randint(0, 255, size=(3,))
-			color = [int(c) for c in color]
-			cv2.rectangle(output, (startX, startY), (endX, endY), color, 2)
 
+			cropped_image = read[int(startY):int(endY), int(startX):int(endX)]
 
-			cropped_image = read[int(startX):int(endX), int(startY):int(endY)]
-
-			cv2.imshow("Image", cropped_image)
-			cv2.waitKey(0)
-
-		# print(points)
-
-			# with open(txt_path+ '/'+ str(image[0:6]) + '.txt', 'a+') as f:
-			# 	# for i in points:
-			# 	f.write(str(startX) + " " + str(endX) + " " + str(startY) + " " + str(endY) +"\n")
-
-		break
-	# break
-	# show the output image
-	# cv2.imshow("Image", output)
-	# cv2.waitKey(0)
-	# plt.figure()
-	# plt.imshow(output)
-	# plt.show()
-
-# cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-# image = cv2.resize(image, (512, 512))
-# saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
-# (success, saliencyMap) = saliency.computeSaliency(image)
-# saliencyMap = (saliencyMap * 255).astype("uint8")
-# cv2.imshow("Image", image)
-# cv2.imshow("Output", saliencyMap)
-# cv2.waitKey(0)
-
-# saliency = cv2.saliency.StaticSaliencyFineGrained_create()
-# (success, saliencyMap) = saliency.computeSaliency(image)
- 
-# # if we would like a *binary* map that we could process for contours,
-# # compute convex hull's, extract bounding boxes, etc., we can
-# # additionally threshold the saliency map
-# threshMap = cv2.threshold(saliencyMap, 0, 255,
-# 	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
- 
-# # show the images
-# # cv2.imshow("Image", image)
-# cv2.imshow("Output", saliencyMap)
-# cv2.imshow("Thresh", threshMap)
-# cv2.waitKey(0)
+			if j<10:
+				cv2.imwrite(cropped_path + '/' + image[0:2] + '000' + str(j) + '.jpg', cropped_image)
+			elif 10<=j<=99:
+				cv2.imwrite(cropped_path + '/' + image[0:2] + '00' + str(j) + '.jpg', cropped_image)
+			elif 100<=j<=999:
+				cv2.imwrite(cropped_path + '/' + image[0:2] + '0' + str(j) + '.jpg', cropped_image)
+			elif j>=1000:
+				cv2.imwrite(cropped_path + '/' + image[0:2] + str(j) + '.jpg', cropped_image)
+			j+=1
